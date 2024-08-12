ef26949123acb7960c563537057c839f70f9029c
Kim_Kevin <122615332+kimkevin-cloud@users.noreply.github.com>
2024-08-12 19:36:30 +0200

    Main geändert für Test

--- muehle/src/muehle/Main.java
+++ muehle/src/muehle/Main.java
@@ -1,9 +1,27 @@
 package muehle;
 
 public class Main {
-	// Test
 	public static void main(String[] args) {
-		System.out.println("Hallo Welt(Kims Test)");
-	}
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
 
+		System.out.println("Einfacher Rechner");
+		System.out.print("Geben Sie die erste Zahl ein: ");
+		int num1 = scanner.nextInt();
+
+		System.out.print("Geben Sie die zweite Zahl ein: ");
+		int num2 = scanner.nextInt();
+
+		System.out.print("Wählen Sie die Operation (1 für Addition, 2 für Subtraktion): ");
+		int operation = scanner.nextInt();
+
+		if (operation == 1) {
+			System.out.println("Ergebnis der Addition: " + (num1 + num2));
+		} else if (operation == 2) {
+			System.out.println("Ergebnis der Subtraktion: " + (num1 - num2));
+		} else {
+			System.out.println("Ungültige Operation");
+		}
+
+		scanner.close();
+	}
 }
